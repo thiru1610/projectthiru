bab56dc4ff9b97e401722a8f7555d33901ca26e0
sivashanmugam09 <thiru1610@gmail.com>
2021-09-25 16:21:49 +0530

    add nes lines

--- src/org/gitsample/sample.java
+++ src/org/gitsample/sample.java
@@ -1,6 +1,10 @@
 package org.gitsample;
 
 public class sample {
+	
+	private void t2() {
+System.out.println("thiru2 started");
+	}
 public static void main(String[] args) {
 	System.out.println("thiru");
 	System.out.println("thirumalai");
