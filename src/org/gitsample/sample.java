148dad404f67bdee00438ed366e0d0572399b871
sivashanmugam09 <thiru1610@gmail.com>
2021-09-25 16:09:49 +0530

    add new lines

--- src/org/gitsample/sample.java
+++ src/org/gitsample/sample.java
@@ -1,6 +1,10 @@
 package org.gitsample;
 
 public class sample {
+	
+	private void t1() {
+System.out.println("thiru1 satrted");
+	}
 public static void main(String[] args) {
 	System.out.println("thiru");
 	System.out.println("thirumalai");
