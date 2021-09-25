c4bf75eea0af372fcc01448f13232e4091d33c2f
sivashanmugam09 <thiru1610@gmail.com>
2021-09-25 15:04:35 +0530

    add lines to of code

---
+++ src/org/gitsample/sample.java
@@ -0,0 +1,9 @@
+package org.gitsample;
+
+public class sample {
+public static void main(String[] args) {
+	System.out.println("thiru");
+	System.out.println("thirumalai");
+	System.out.println("thirumalairajan");
+}
+}
