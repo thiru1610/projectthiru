dc0f7916a5e33a25ae5c0a3a2961aa370b5e9dbb
sivashanmugam09 <thiru1610@gmail.com>
2021-09-26 15:55:08 +0530

    thiru3 added

--- src/org/gitsample/sample.java
+++ src/org/gitsample/sample.java
@@ -1,9 +1,14 @@
 package org.gitsample;
 
 public class sample {
-public static void main(String[] args) {
-	System.out.println("thiru");
-	System.out.println("thirumalai");
-	System.out.println("thirumalairajan");
-}
+
+	private void t3() {
+		System.out.println("thiru3 added his work");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("thiru");
+		System.out.println("thirumalai");
+		System.out.println("thirumalairajan");
+	}
 }
